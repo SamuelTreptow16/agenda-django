1cd1c6e660b007ac22aca34a266f44a0e0985525
samueltreptow <pessoal.swtreptow@gmail.com>
2024-12-27 21:25:53 -0300

    Criando senha superuser do Django

--- project/settings.py
+++ project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES = (
-    BASE_DIR / 'static',
+STATICFILES_DIRS = (
+    BASE_DIR / 'base_static',
 )
 
 # Default primary key field type
